4285e61e2b2322c148faf8c85ca5d5d83da631c0
grkmr <goerkem.emre.oeztuerk@gmail.com>
2025-07-29 12:26:10 +0200

    :bug: Fix abort error

--- src/frontend/api/fetcher.ts
+++ src/frontend/api/fetcher.ts
@@ -2,20 +2,33 @@ export const customFetch = async <T>(
   url: string,
   options: RequestInit,
 ): Promise<T> => {
-  const response = await fetch(url, { ...options, credentials: "include" });
+  let response: Response;
+
+  try {
+    response = await fetch(url, { ...options, credentials: "include" });
+  } catch (error: any) {
+    if (error.name === "AbortError") {
+      console.warn("Request aborted");
+      return Promise.reject(error);
+    }
+    throw error;
+  }
 
   const contentType = response.headers.get("content-type") || "";
 
   let data: any;
-
   try {
     if (contentType.includes("application/json")) {
       data = await response.json();
     } else {
-      data = await response.text(); // fallback
+      data = await response.text();
     }
-  } catch (e) {
-    console.error("Error parsing response body:", e);
+  } catch (error: any) {
+    if (error.name === "AbortError") {
+      console.warn("Body parsing aborted");
+      return Promise.reject(error);
+    }
+    console.error("Error parsing response body:", error);
     data = null;
   }
 
