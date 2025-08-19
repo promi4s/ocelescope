c2a60125152f065520cc0cf3e2efc3d4c1751647
grkmr <goerkem.emre.oeztuerk@gmail.com>
2025-08-19 14:07:52 +0200

    :bug: Fix removal of extension in template

--- template/hooks/post_gen_project.py
+++ template/hooks/post_gen_project.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 from pathlib import Path
 
-# Cookiecutter renders this variable into the script before running
 include_example = "{{ cookiecutter.include_example_extension|lower }}"
 
 # Build the path to the example extension
-example_file = Path("src") / "plugin" / "extensions" / "example.py"
+example_file = (
+    Path("src") / "{{cookiecutter.__package_name}}" / "extensions" / "example.py"
+)
 
 if include_example != "y":
     if example_file.exists():
