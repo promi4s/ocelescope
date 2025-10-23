93edbaa22aa3a56aacc0679c92d0453208b1a10c
grkmr <goerkem.emre.oeztuerk@gmail.com>
2025-10-23 17:15:52 +0200

    :sparkles: Plotly viewer

---
+++ src/frontend/components/Resources/Viewer/Viewers/plotly.tsx
@@ -0,0 +1,32 @@
+import { ComponentProps } from "react";
+import { VisualizationProps } from "..";
+import dynamic from "next/dynamic";
+
+const Plot = dynamic(() => import("react-plotly.js"), { ssr: false });
+
+const PlotlyViewer: React.FC<VisualizationProps<"plotly">> = ({
+  visualization,
+  isPreview,
+}) => {
+  const { data, layout } = visualization.data as Pick<
+    ComponentProps<typeof Plot>,
+    "data" | "layout"
+  >;
+
+  return (
+    <Plot
+      data={data}
+      layout={{ ...layout, width: undefined, height: undefined }}
+      config={{
+        responsive: true,
+        displaylogo: false,
+        staticPlot: !!isPreview,
+        showTips: false,
+      }}
+      useResizeHandler={true}
+      style={{ width: "100%", height: "100%" }}
+    />
+  );
+};
+
+export default PlotlyViewer;
