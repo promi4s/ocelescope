bf72141f75ef1624a9089695b30c2fd6a7938e05
Görkem-Emre Öztürk <32603164+Grkmr@users.noreply.github.com>
2026-03-24 12:47:37 +0100

    :recycle: Move pnpm workspace to root (#37)

--- src/frontend/packages/core/tsdown.config.ts
+++ src/frontend/packages/core/tsdown.config.ts
@@ -1,15 +1,12 @@
-import { mergeCssPlugin } from "@ocelescope/merge-css-plugin";
 import { defineConfig } from "tsdown";
-import LightningCSS from "unplugin-lightningcss/rolldown";
 export default defineConfig({
   platform: "neutral",
-  entry: ["src/index.ts", "src/styles/styles.css"],
-  plugins: [
-    LightningCSS({
-      options: {
-        minify: true,
-      },
-    }),
-    mergeCssPlugin("styles.css"),
-  ],
+  entry: ["src/index.ts", "src/styles.css"],
+  css: {
+    modules: {
+      scopeBehaviour: "local",
+      generateScopedName: "[hash]_[local]",
+      localsConvention: "camelCase",
+    },
+  },
 });
