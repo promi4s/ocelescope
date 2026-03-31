0cf4670d04bbc544133e5efaed1f60f1ad87e74a
grkmr <goerkem@posteo.com>
2026-03-31 17:46:06 +0200

    :construction:

--- src/frontend/modules/ocelot/src/routes/logOverview.tsx
+++ src/frontend/modules/ocelot/src/routes/logOverview.tsx
@@ -1,7 +1,8 @@
-import { LoadingOverlay } from "@mantine/core";
+import { Grid, LoadingOverlay } from "@mantine/core";
 import { defineModuleRoute, useCurrentOcel } from "@ocelescope/core";
 import OCELInfo from "../components/OcelInfo";
 import AttributeTable from "../components/AttributeTable";
+import { OCELEntityBarList } from "../components/OCELEntityBarList";
 
 const LogOverviewPage = () => {
   const { id } = useCurrentOcel();
@@ -9,10 +10,17 @@ const LogOverviewPage = () => {
     return <LoadingOverlay />;
   }
   return (
-    <>
-      <OCELInfo ocelId={id} />
-      <AttributeTable ocelId={id} />
-    </>
+    <Grid>
+      <Grid.Col span={12}>
+        <OCELInfo ocelId={id} />
+      </Grid.Col>
+      <Grid.Col span={12}>
+        <AttributeTable ocelId={id} />
+      </Grid.Col>
+      <Grid.Col span={6}>
+        <OCELEntityBarList type="events" ocelId={id} />
+      </Grid.Col>
+    </Grid>
   );
 };
 
