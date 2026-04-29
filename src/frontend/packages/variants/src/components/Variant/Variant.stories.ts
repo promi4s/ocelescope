091cd217dd3738d49ab9fc327a94a47308825609
Görkem-Emre Öztürk <32603164+Grkmr@users.noreply.github.com>
2026-04-29 15:35:36 +0200

    :sparkles: Variant component (#75)

---
+++ src/frontend/packages/variants/src/components/Variant/Variant.stories.ts
@@ -0,0 +1,32 @@
+import { faker } from "@faker-js/faker";
+import type { Meta, StoryObj } from "@storybook/react-vite";
+
+import { Variant } from "./Variant";
+
+faker.seed(4);
+
+const meta = {
+  title: "Variant",
+  component: Variant,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Variant>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const generateVariant = (length: number) => {
+  const activityArrays = Array.from({ length: 5 }).map(() =>
+    faker.lorem.word(),
+  );
+
+  return Array.from({ length: 10 }).map(() =>
+    faker.helpers.arrayElement(activityArrays),
+  );
+};
+
+export const Default: Story = {
+  args: { variant: generateVariant(10) },
+};
