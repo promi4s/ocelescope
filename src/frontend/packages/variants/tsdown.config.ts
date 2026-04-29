c17af9477f6a0bfb7471ec1539afc51e8d20feeb
grkmr <goerkem@posteo.com>
2026-04-29 15:43:51 +0200

    :bug: Fix tsdown config

--- src/frontend/packages/variants/tsdown.config.ts
+++ src/frontend/packages/variants/tsdown.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   platform: "neutral",
-  entry: ["src/index.ts", "src/styles.css"],
+  entry: ["src/index.ts"],
   css: {
     modules: {
       scopeBehaviour: "local",
