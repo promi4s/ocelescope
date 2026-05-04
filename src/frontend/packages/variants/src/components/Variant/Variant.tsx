0f6bff4201d13bb352239a85fe77beb205f7380b
grkmr <goerkem@posteo.com>
2026-05-04 11:57:03 +0200

    :sparkles: Grouping

--- src/frontend/packages/variants/src/components/Variant/Variant.tsx
+++ src/frontend/packages/variants/src/components/Variant/Variant.tsx
@@ -1,11 +1,13 @@
 import { generateColor } from "@marko19907/string-to-color";
+import { XIcon } from "lucide-react";
 import { useMemo } from "react";
 import styles from "./Variant.module.css";
 
 export const Variant: React.FC<{
   variant: string[];
   colors?: Record<string, string>;
-}> = ({ variant, colors = {} }) => {
+  enableGrouping?: boolean;
+}> = ({ variant, colors = {}, enableGrouping = false }) => {
   const colorMap = useMemo(
     () => ({
       ...colors,
@@ -18,15 +20,49 @@ export const Variant: React.FC<{
     [variant, colors],
   );
 
+  const groupedVariants = useMemo(
+    () =>
+      variant.reduce(
+        (acc, curr) => {
+          const lastElement = acc.at(-1);
+
+          if (
+            !enableGrouping ||
+            !lastElement ||
+            lastElement.activity !== curr
+          ) {
+            return [...acc, { activity: curr, freq: 1 }];
+          }
+
+          return [
+            ...acc.slice(0, -1),
+            { ...lastElement, freq: lastElement.freq + 1 },
+          ];
+        },
+        [] as { activity: string; freq: number }[],
+      ),
+    [variant, enableGrouping],
+  );
+
   return (
     <ul className={styles.variant}>
-      {variant.map((activity, index) => (
+      {groupedVariants.map(({ freq, activity }, index) => (
         <li
           key={`${activity}_${index}`}
           className={styles.item}
-          style={{ "--c": colorMap[activity] } as React.CSSProperties}
+          style={
+            {
+              "--c": colorMap[activity],
+            } as React.CSSProperties
+          }
         >
-          <span>{activity}</span>
+          {freq > 1 && (
+            <>
+              <span>{freq}</span>
+              <XIcon size={14} />
+            </>
+          )}
+          <span className={styles.itemLabel}>{activity}</span>
         </li>
       ))}
     </ul>
