5cdcba58ec218657c6a4aaffff1705c0e529ba12
grkmr <goerkem@posteo.com>
2026-05-12 13:41:49 +0200

    :recycle: Move logOverview to its own module

--- src/frontend/modules/log-overview/src/index.ts
+++ src/frontend/modules/log-overview/src/index.ts
@@ -1,9 +1,10 @@
 import { defineModule } from "@ocelescope/core";
+import logOverview from "./pages/logOverview";
 
 export default defineModule({
   name: "logOverview",
   description: "A tool for inspecting OCELs",
   label: "Log Overview",
   authors: [{ name: "Öztürk, Görkem-Emre" }],
-  routes: [],
+  routes: [logOverview],
 });
