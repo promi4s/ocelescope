0b4b1472c751ad3290cb579e8871e6e77b5e38a3
Görkem-Emre Öztürk <32603164+Grkmr@users.noreply.github.com>
2026-05-12 15:47:36 +0200

    :recycle: Move LogOverview into its own module (#90)

--- src/frontend/app/ocelescope.config.ts
+++ src/frontend/app/ocelescope.config.ts
@@ -1,9 +1,10 @@
 import type { OcelescopeConfig } from "@ocelescope/core";
 import filter from "@ocelescope/filter";
+import overview from "@ocelescope/log-overview";
 import management from "@ocelescope/management";
 import ocelot from "@ocelescope/ocelot";
 import plugin from "@ocelescope/plugin";
 
 export default {
-  modules: [management, plugin, filter, ocelot],
+  modules: [management, overview, plugin, filter, ocelot],
 } satisfies OcelescopeConfig;
