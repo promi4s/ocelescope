3ec52fe6be844d8f90e7ca71fc5850a11646e968
grkmr <goerkem@posteo.com>
2026-05-12 13:30:16 +0200

    :sparkles: Log Overview module

---
+++ src/frontend/modules/log-overview/src/index.ts
@@ -0,0 +1,9 @@
+import { defineModule } from "@ocelescope/core";
+
+export default defineModule({
+  name: "logOverview",
+  description: "A tool for inspecting OCELs",
+  label: "Log Overview",
+  authors: [{ name: "Öztürk, Görkem-Emre" }],
+  routes: [],
+});
