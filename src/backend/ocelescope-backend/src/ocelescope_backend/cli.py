78434e29d316e36baec4d57d1d2f18e458b97f4e
grkmr <goerkem@posteo.com>
2026-05-13 14:09:36 +0200

    :technologist: Pass all uvicorn parameters

--- src/backend/ocelescope-backend/src/ocelescope_backend/cli.py
+++ src/backend/ocelescope-backend/src/ocelescope_backend/cli.py
@@ -1,10 +1,20 @@
-import uvicorn
+import sys
+
+from uvicorn.main import main as uvicorn_main
 
 
 def main() -> None:
-    uvicorn.run(
+    extra_args = sys.argv[1:]
+
+    if "--host" not in extra_args:
+        extra_args = ["--host", "0.0.0.0", *extra_args]
+
+    if "--port" not in extra_args:
+        extra_args = ["--port", "8000", *extra_args]
+
+    sys.argv = [
+        "uvicorn",
         "ocelescope_backend.main:app",
-        host="0.0.0.0",
-        port=8000,
-        reload=True,
-    )
+        *extra_args,
+    ]
+    uvicorn_main()
