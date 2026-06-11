6bc53b10961c1f031436323c4c746e2e92d86651
grkmr <goerkem@posteo.com>
2026-06-11 23:16:36 +0200

    :sparkles: useDatatable

--- src/frontend/modules/filter/src/hooks/useDatatable.tsx
+++ src/frontend/modules/filter/src/hooks/useDatatable.tsx
@@ -14,7 +14,6 @@ export const useDatatable = <T extends Record<string, any>>({
   data = [],
   columnNames,
   defaultSorted,
-  pageSize = 20,
 }: useDatatableProps<T>) => {
   const [sortStatus, setSortStatus] = useState<DataTableSortStatus<T>>({
     columnAccessor: defaultSorted,
@@ -25,23 +24,16 @@ export const useDatatable = <T extends Record<string, any>>({
     Partial<Record<keyof T, string[]>>
   >({});
 
-  const [page, setPage] = useState<number>(0);
-
   const records = useMemo(() => {
     return sortRecords(
       data,
       sortStatus.columnAccessor as keyof T,
       sortStatus.direction,
-    )
-      .filter((record) =>
-        Object.entries(columnSelection).every(
-          ([a, b]) => !b || b.length === 0 || b.includes(record[a]),
-        ),
-      )
-      .slice(
-        page * Math.floor(data.length / pageSize),
-        (page + 1) * Math.floor(data.length / pageSize),
-      );
+    ).filter((record) =>
+      Object.entries(columnSelection).every(
+        ([a, b]) => !b || b.length === 0 || b.includes(record[a]),
+      ),
+    );
   }, [sortStatus, columnSelection, data]);
 
   const columns = useMemo(
