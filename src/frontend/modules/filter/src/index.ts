8366dd6c2f1abde64872c1328e62b31650d0a087
Görkem-Emre Öztürk <32603164+grkmr@users.noreply.github.com>
2026-06-17 15:13:57 +0200

    :sparkles: Navbar grouping (#171)

--- src/frontend/modules/filter/src/index.ts
+++ src/frontend/modules/filter/src/index.ts
@@ -6,7 +6,7 @@ import FilterPage from "./components/FilterPage";
 export default defineModule({
   name: "filter",
   description: "Filter functionality provided by the ocelescope package",
-  label: "Filters",
+  label: "Log Filter",
   authors: [{ name: "Öztürk, Görkem-Emre" }],
   routes: [FilterPage],
   icon: FilterIcon,
