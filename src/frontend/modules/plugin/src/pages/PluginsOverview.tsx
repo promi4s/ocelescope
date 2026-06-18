8087c5d45c54c4269d416fe326ce76f3292505c1
grkmr <goerkem@posteo.com>
2026-06-18 12:42:51 +0200

    :sparkles: Upload plugins section

--- src/frontend/modules/plugin/src/pages/PluginsOverview.tsx
+++ src/frontend/modules/plugin/src/pages/PluginsOverview.tsx
@@ -1,24 +1,33 @@
-import { SimpleGrid } from "@mantine/core";
+import { SimpleGrid, Stack, Text, Title } from "@mantine/core";
 import { usePlugins } from "@ocelescope/api-base";
 import {
   PluginCard,
   UploadPluginCard,
 } from "../components/PluginCard/PluginCard";
+import { PluginUploadSection } from "../components/PluginUploadSection/PluginUploadSection";
 
 const PluginsOverview: React.FC = () => {
-  const { data: plugins = [] } = usePlugins();
+  const { data: plugins } = usePlugins();
 
   return (
-    <SimpleGrid
-      cols={{ base: 1, sm: 2, md: 3, lg: 3 }}
-      spacing={{ base: 10, sm: "xl" }}
-      verticalSpacing={{ base: "md", sm: "xl" }}
-    >
-      {plugins.map((plugin) => (
-        <PluginCard key={plugin.id} plugin={plugin} />
-      ))}
-      <UploadPluginCard />
-    </SimpleGrid>
+    <Stack>
+      <Title order={2}>Plugin Overview</Title>
+      <Text></Text>
+      {plugins && plugins.length === 0 ? (
+        <PluginUploadSection />
+      ) : (
+        <SimpleGrid
+          cols={{ base: 1, sm: 2, md: 3, lg: 3 }}
+          spacing={{ base: 10, sm: "xl" }}
+          verticalSpacing={{ base: "md", sm: "xl" }}
+        >
+          {plugins?.map((plugin) => (
+            <PluginCard key={plugin.id} plugin={plugin} />
+          ))}
+          <UploadPluginCard />
+        </SimpleGrid>
+      )}
+    </Stack>
   );
 };
 
