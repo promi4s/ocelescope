8fa5eecafe5282808d8774153b6263b39eba2e87
Görkem-Emre Öztürk <32603164+grkmr@users.noreply.github.com>
2026-07-01 16:01:00 +0200

    :bug: Don't transpile non modules in app (#276)

--- src/frontend/app/next.config.ts
+++ src/frontend/app/next.config.ts
@@ -11,11 +11,8 @@ const nextConfig: NextConfig = {
   transpilePackages: [
     "@mantine/charts",
     "recharts",
-    "@ocelescope/core",
     "@ocelescope/resources",
     "@ocelescope/plugin",
-    "@ocelescope/api-base",
-    "@ocelescope/api-client",
     "@ocelescope/filter",
     "@ocelescope/discovery",
     "@ocelescope/log-overview",
